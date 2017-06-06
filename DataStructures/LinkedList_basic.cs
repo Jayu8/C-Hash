ab000c0869f53541706fca27c751241beffb33a5
Jayu8 <narrajayakrishna@outlook.com>
2017-06-06 16:56:56 -0400

    Linked List

---
+++ DataStructures/LinkedList_basic.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Linked_List_Basic
+{
+    /// <summary>
+    /// This class creates an object NODE with data members int data and Node next
+    /// Here Node next implies an instance of Node class where the next object is referenced.
+    /// IMPORTANT: It's not node inside node but the chain of nodes. 
+    /// This implies a NODE consists a address which is a refernece to new node.  
+    /// </summary>
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int i)
+        {
+            data = i;
+            next = null;
+        }
+        public void print()
+        {
+            Console.WriteLine("the data is " + data);  // This only prints the first node 
+            if (next!=null)
+            {
+                //here print method can be called as next is an object of NODE class too similar to mynode
+                //This statement calls this print function recursively until the line ends
+                next.print(); 
+             }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node mynode = new Node(7);
+            // Next can be accesses here as mynode is object of class NODE and hence access to data members
+            //Here mynode.data can also be accessed
+            mynode.next = new Node(11);  
+            mynode.next.next = new Linked_List_Basic.Node(8); // this is similar to Node(8)
+            mynode.print(); 
+        }
+    }
+}
