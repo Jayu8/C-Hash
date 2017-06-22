5f46654d6dcd4fe6439ae7888fe3ccf6d87f91a3
Jayu8 <narrajayakrishna@outlook.com>
2017-06-22 12:38:34 -0400

    Create Linked_List_Basic.cs

---
+++ Two Projects DLL creation/Linked_List_Basic.cs
@@ -0,0 +1,16 @@
+"""
+First create a new project (Class Library)
+Go to this project Linked_List_Basic->RightClick->Add->Reference->
+Select the other project: ClassLibrary1
+"""
+
+//Now you can use this
+using ClassLibrary1;   
+
+namespace Linked_List_Basic
+{
+    public class Class1
+    {
+        a=1;
+    }
+}
